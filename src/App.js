973455c4b9f83e70667704d42797c1165f8fdb91
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-06-15 03:52:31 +0300

    updating lab 07

--- src/App.js
+++ src/App.js
@@ -6,50 +6,65 @@ class App extends React.Component {
     super(props);
     this.state = {
       locationData: "",
+      weatherDataDetails: "",
+      weatherData: "",
       errorMessage: "",
-      displayErrorMessage:false,
-      displayMap:false,
+      displayErrorMessage: false,
+      displayMap: false,
     };
   }
 
   getLocations = async (event) => {
     event.preventDefault();
     let searchQuery = event.target.searchQuery.value;
-    let locationURL = `https://eu1.locationiq.com/v1/search.php?key=pk.acee6642223badb53b46f2eafaac6fd5&q=${searchQuery}&format=json`;
 
     try {
-      let locationAxios = await axios.get(locationURL);
+      let locationAxios = await axios.get(
+        `https://eu1.locationiq.com/v1/search.php?key=pk.acee6642223badb53b46f2eafaac6fd5&q=${searchQuery}&format=json`
+      );
+      let weatherAxios = await axios.get(
+        `${process.env.REACT_APP_URL}/weather`
+      );
       this.setState({
         displayErrorMessage: false,
-        
+        weatherDataDetails:weatherAxios.data,
+        weatherData: weatherAxios.data.data,
         locationData: locationAxios.data[0],
-        displayMap:true,
+        displayMap: true,
       });
     } catch {
       this.setState({
         displayErrorMessage: true,
-        displayMap:false,
+        displayMap: false,
         errorMessage: "ERROR! This is bad respons",
       });
     }
+    console.log(this.state.weatherData);
   };
 
   render() {
     return (
       <div>
-        <h1>axious lab</h1>
+        <h1>city-explorer-app</h1>
         <form onSubmit={this.getLocations}>
           <input type="text" placeholder="city name" name="searchQuery"></input>
           <input type="submit" value="search"></input>
         </form>
-        <p>{this.state.locationData.display_name}</p>
-        <p>{this.state.locationData.lat}</p>
-        <p>{this.state.locationData.lon}</p>
-        {this.state.displayMap && <img onSubmit={this.getLocations}
-        src = {`https://maps.locationiq.com/v3/staticmap?key=pk.acee6642223badb53b46f2eafaac6fd5&markers=icon:%3Cicon%3E|${this.state.locationData.lat},${this.state.locationData.lon}`} 
-        alt={`map`}
-        />}
-        
+        <p>City Information: {this.state.locationData.display_name}</p>
+        <p>Latitude: {this.state.locationData.lat}</p>
+        <p>Longitude: {this.state.locationData.lon}</p>
+        {this.state.displayMap && (
+          <img
+            onSubmit={this.getLocations}
+            src={`https://maps.locationiq.com/v3/staticmap?key=pk.acee6642223badb53b46f2eafaac6fd5&markers=icon:%3Cicon%3E|${this.state.locationData.lat},${this.state.locationData.lon}`}
+            alt={`map`}
+          />
+        )}
+        <h1>Weather Data</h1>
+        <p>City Name: {this.state.weatherDataDetails.city_name}</p>
+        <p>City Code: {this.state.weatherDataDetails.country_code}</p>
+        <p>Weather information: {JSON.stringify(this.state.weatherData[0])}</p>
+
         {this.state.displayErrorMessage && this.state.errorMessage}
       </div>
     );
